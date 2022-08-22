99df9933975f782a52ee42168889467ce53b4841
Lisa_Lyakh <99537140+LisaLyakh@users.noreply.github.com>
2022-08-22 13:26:43 +0300

    update 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,15 +1,14 @@
+const changeColorBtn = document.querySelector('button.change-color');
+const body = document.querySelector('body');
+const nameColor = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  nameColor.textContent = color;
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-const bodyEl = document.querySelector("body");
-
-const changeColorEl = document.querySelector(".change-color");
-
-const colorEl = document.querySelector(".color");
-
-changeColorEl.addEventListener("click", changeBgColorRandom);
-
-function changeBgColorRandom() {
-  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
-  colorEl.textContent = getRandomHexColor();
 }
